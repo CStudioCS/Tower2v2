f67aaba9e33a07414500373aedcf9d9dc50c028c
fares <84095553+Fypur@users.noreply.github.com>
2026-02-08 16:25:08 +0100

    Player acceleration and friction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float speed = 7f;
+    [SerializeField] private float maxSpeed = 8f;
+    [SerializeField] private float acceleration = 100f;
+    [SerializeField] private float friction = 140f;
 
     [Header("References")]
     [SerializeField] private Player player;
@@ -25,7 +27,40 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = Vector2.zero;
             return;
         }
-        
-        rb.linearVelocity = moveAction.ReadValue<Vector2>() * speed;
+
+        rb.linearVelocity = VelocityApproach();
+    }
+
+
+    //this is adapted from code from Unnamed check it out on https://fypur.itch.io/unnamed (tsais le mec qui fait sa pub sans aucune honte)
+    /// <summary>
+    /// Make your character accelerate or use friction depending on player input and current speed
+    /// </summary>
+    private Vector2 VelocityApproach()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+
+        //We wanna move and we're not at top speed -> accelerate
+        if (move != Vector2.zero && rb.linearVelocity.sqrMagnitude < maxSpeed * maxSpeed)
+        {
+            //Account for the fact that move can be of norm different than one (for controllers when moving slowly)
+            Vector2 apporached = (move.sqrMagnitude > 0.3f * 0.3f ? move.normalized : move) * maxSpeed;
+            return Approach(rb.linearVelocity, apporached, acceleration * Time.deltaTime);
+        }
+
+        //We don't wanna move or we're at max speed -> friction (friction is just reverse acceleration, it's not a multiple of velocity)
+        return Approach(rb.linearVelocity, Vector2.zero, friction * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Returns approached version of <paramref name="value"/> towards <paramref name="approached"/> with a step size of <paramref name="move"/>
+    /// </summary>
+    private Vector2 Approach(Vector2 value, Vector2 approached, float move)
+    {
+        Vector2 dir = (approached - value);
+        float maxDisplacement = dir.magnitude;
+        dir.Normalize();
+
+        return value + dir * Mathf.Min(maxDisplacement, move);
     }
 }
