1ecea9c52bec599d9f2c819c1d4a8989eec9eafa
CedrakNedrak <126580442+CedrakNedrak@users.noreply.github.com>
2026-02-08 01:08:48 +0100

    Amélioration du nouveau fonctionnement du menu

--- Assets/Scripts/Menu/ButonPlayOnClick.cs
+++ Assets/Scripts/Menu/ButonPlayOnClick.cs
@@ -7,7 +7,6 @@ using DG.Tweening;
 
 public class ButtonClicked : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private GameObject buttonPlay;
     
     [SerializeField] private GameObject buttonSettings;
@@ -16,55 +15,54 @@ public class ButtonClicked : MonoBehaviour
 
     [SerializeField] private Camera cam;
 
+    [SerializeField] private float finalPositionButtonAfterMove = 15f;
 
+    
+    [SerializeField] private float speedButtonWhenClicked = 0.8f;
 
     private float initialCamSize;
     private Vector3 initialPositionButonSettings;
     private Vector3 initialCamPosition;
-    private float posyFinal = 0f;
+    private float posYFinal;
 
     void Start()
     {
-        // Store initial camera parameters
         initialCamSize = cam.orthographicSize;
         initialCamPosition = cam.transform.position;
-
         initialPositionButonSettings = buttonSettings.gameObject.transform.position;
     }
 
     public void PlayButtonClicked()
     {
-        // Load the game scene
-        buttonPlay.transform.DOMoveX(15f, 0.8f).SetEase(Ease.OutQuad);
-        posyFinal = buttonPlay.transform.position.y;
-        StartCoroutine(Zoom.TransitionZoomInCoroutine(cam, initialCamSize, initialCamPosition, posyFinal,SceneManager.LoadScene));
-        
+        //Utilisation de DOTween 
+        buttonPlay.transform.DOMoveX(finalPositionButtonAfterMove, speedButtonWhenClicked).SetEase(Ease.OutQuad);
+        posYFinal = buttonPlay.transform.position.y;
+        StartCoroutine(Zoom.TransitionZoomInCoroutine(cam, initialCamSize, initialCamPosition, posYFinal,SceneManager.LoadScene));
     }
 
     public void SettingsButtonClicked()
     {
-        // Load the settings scene
-        
-        buttonSettings.transform.DOMoveX(15f, 0.8f).SetEase(Ease.OutQuad);
-        posyFinal = buttonSettings.transform.position.y;
-        StartCoroutine(Zoom.TransitionZoomInCoroutine(cam, initialCamSize, initialCamPosition, posyFinal));
+        //Utilisation de DOTween 
+        buttonSettings.transform.DOMoveX(finalPositionButtonAfterMove, speedButtonWhenClicked).SetEase(Ease.OutQuad);
+        posYFinal = buttonSettings.transform.position.y;
+        StartCoroutine(Zoom.TransitionZoomInCoroutine(cam, initialCamSize, initialCamPosition, posYFinal));
     }
 
     public void QuitButtonClicked()
     {
+        //Utilisation de DOTween 
+        buttonQuit.transform.DOMoveX(finalPositionButtonAfterMove, speedButtonWhenClicked).SetEase(Ease.OutQuad);
+        posYFinal = buttonQuit.transform.position.y;
+        StartCoroutine(Zoom.TransitionZoomInCoroutine(cam, initialCamSize, initialCamPosition, posYFinal));
         
-        buttonQuit.transform.DOMoveX(15f, 0.8f).SetEase(Ease.OutQuad);
-        posyFinal = buttonQuit.transform.position.y;
-        StartCoroutine(Zoom.TransitionZoomInCoroutine(cam, initialCamSize, initialCamPosition, posyFinal));
-        // Quit the application
         Application.Quit();
     }
 
     public void QuitSettingsButtonClicked()
     {
-        // Load the main menu scene
-        buttonSettings.transform.DOMoveX(0f, 0.8f).SetEase(Ease.InQuad);
-        posyFinal = initialPositionButonSettings.y;
-        StartCoroutine(Zoom.TransitionZoomOutCoroutine(cam, initialCamSize, initialCamPosition, posyFinal));
+        //Utilisation de DOTween 
+        buttonSettings.transform.DOMoveX(initialPositionButonSettings.x, speedButtonWhenClicked).SetEase(Ease.InQuad);
+
+        StartCoroutine(Zoom.TransitionZoomOutCoroutine(cam, initialCamSize, initialCamPosition, initialPositionButonSettings.y));
     }
 }
--- Assets/Scripts/Menu/Zoom.cs
+++ Assets/Scripts/Menu/Zoom.cs
@@ -3,44 +3,64 @@ using System.Collections;
 using System;
 public static class Zoom
 {
-    private static void ZoomIn(Camera cam, float initialCamSize, Vector3 initialCamPosition, float time, float posyFinal)
+    private const float zoomDuration = 1f;
+    private const float zoomedInSize = 0.5f;
+    
+    private static float posZfinal;
+    private static float posXfinal;
+      
+    public static IEnumerator TransitionZoomInCoroutine(Camera cam, float initialCamSize, Vector3 initialCamPosition, float posYFinal, Action<string>  action = null)
     {
-        cam.orthographicSize = Mathf.Lerp(initialCamSize, 0.5f, Easing.QuadOut(time));
-        
-        cam.transform.position = Vector3.Lerp(initialCamPosition, new Vector3(0, posyFinal, -10f), Easing.QuadOut(time));
+    
+        posZfinal = cam.transform.position.z;
+        posXfinal = cam.transform.position.x;
 
-    }   
-
-    public static IEnumerator TransitionZoomInCoroutine(Camera cam, float initialCamSize, Vector3 initialCamPosition, float posyFinal, Action<string>  action = null)
-    {
+        //Gere à chaque frame le zoom de la caméra 
         float time = 0f;
-        while (time < 1f)
+        while (time < zoomDuration)
         {
             time += Time.deltaTime;
-            ZoomIn(cam, initialCamSize, initialCamPosition, time, posyFinal);
+            ZoomIn(cam, initialCamSize, initialCamPosition, time, posYFinal);
             yield return null;
             
         }
+
+        //Utiliser uniquement pour le play et evite de réecrire la fonction de zoom uniquement pour le play
         action?.Invoke("Game");
     }
 
-    private static void ZoomOut(Camera cam, float initialCamSize, Vector3 initialCamPosition, float time, float posyFinal)
+    private static void ZoomIn(Camera cam, float initialCamSize, Vector3 initialCamPosition, float time, float posYFinal)
     {
-        cam.orthographicSize = Mathf.Lerp(0.5f, initialCamSize, Easing.QuadOut(time));
-        
-        cam.transform.position = Vector3.Lerp(new Vector3(0, posyFinal, -10f), initialCamPosition, Easing.QuadOut(time));
+        //Modifie le zoom de la caméra
+        cam.orthographicSize = Mathf.Lerp(initialCamSize, zoomedInSize, Easing.QuadOut(time));
+        //Modifie la position de la camérapour qu'elle zoom au bon endroit 
+        cam.transform.position = Vector3.Lerp(initialCamPosition, new Vector3(posXfinal, posYFinal, posZfinal), Easing.QuadOut(time));
 
-    }  
+    } 
 
-    public static IEnumerator TransitionZoomOutCoroutine(Camera cam, float initialCamSize, Vector3 initialCamPosition, float posyFinal)
+    public static IEnumerator TransitionZoomOutCoroutine(Camera cam, float initialCamSize, Vector3 initialCamPosition, float posYFinal)
     {
+
+        posZfinal = cam.transform.position.z;
+        posXfinal = cam.transform.position.x;
+
+
+        //Gere à chaque frame le zoom de la caméra 
         float time = 0f;
-        while (time < 1f)
+        while (time < zoomDuration)
         {
             time += Time.deltaTime;
-            ZoomOut(cam, initialCamSize, initialCamPosition, time, posyFinal);
+            ZoomOut(cam, initialCamSize, initialCamPosition, time, posYFinal);
             yield return null;
             
         }
     }
+
+    private static void ZoomOut(Camera cam, float initialCamSize, Vector3 initialCamPosition, float time, float posYFinal)
+    {
+        //Modifie le zoom de la caméra
+        cam.orthographicSize = Mathf.Lerp(zoomedInSize, initialCamSize, Easing.QuadOut(time));
+        //Modifie la position de la caméra pour qu'elle zoom au bon endroit
+        cam.transform.position = Vector3.Lerp(new Vector3(posXfinal, posYFinal, posZfinal), initialCamPosition, Easing.QuadOut(time));
+    }  
 }
