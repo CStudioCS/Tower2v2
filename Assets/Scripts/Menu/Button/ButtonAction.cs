9e96927f2f92edc01ff188ed789ba72a2bf5b89e
CedrakNedrak <126580442+CedrakNedrak@users.noreply.github.com>
2026-02-09 19:10:22 +0100

    Remove redundant comment in ButtonAction class
    Removed unnecessary comment about Start method.

--- Assets/Scripts/Menu/Button/ButtonAction.cs
+++ Assets/Scripts/Menu/Button/ButtonAction.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public abstract class ButtonAction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public abstract void Action();
 
     public abstract void Movement();
