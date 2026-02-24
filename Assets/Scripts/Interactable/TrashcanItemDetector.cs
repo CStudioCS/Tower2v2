ff641941257aea4225755d74352e71fb70e74173
Fypur <84095553+Fypur@users.noreply.github.com>
2026-02-24 19:45:02 +0100

    Small correction

--- Assets/Scripts/Interactable/TrashcanItemDetector.cs
+++ Assets/Scripts/Interactable/TrashcanItemDetector.cs
@@ -4,12 +4,9 @@ public class TrashcanItemDetector : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider != null)
+        if(collider != null && collider.gameObject.TryGetComponent<Item>(out Item item))
         {
-            if(collider.gameObject.TryGetComponent<Item>(out Item item))
-            {
-                Destroy(collider.gameObject);
-            }
+            Destroy(collider.gameObject);
         }
     }
 }
