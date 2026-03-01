2161f78f2a787e5345cad137028fe3d298af5835
Thomas Bringer <thomas.bringer@madboxgames.io>
2026-03-01 16:46:39 +0100

    Added empty lines in SettingsButton

--- Assets/Scripts/MainMenu/Button/ParticularButton/SettingsButton.cs
+++ Assets/Scripts/MainMenu/Button/ParticularButton/SettingsButton.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+
 public class SettingsButton : MovingButton
 {
     [SerializeField] private Button quitSettingsButton;
+    
     public override void Action()
     {
         quitSettingsButton.interactable = true;
