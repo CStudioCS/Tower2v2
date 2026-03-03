f25731d91e867a8afb0417f96d2b5091538f01aa
Thomas Bringer <thomas.bringer@madboxgames.io>
2026-03-03 18:35:01 +0100

    Finished 2 VFXs.

--- Assets/Scripts/Recipes/Recipe.cs
+++ Assets/Scripts/Recipes/Recipe.cs
@@ -93,7 +93,6 @@ public class Recipe : MonoBehaviour
 
     private void ReachTarget()
     {
-        Debug.Log($"Recipe {gameObject.name} reaching {TargetPosition}");
         ReachTargetPosition(); ReachTargetScale();
     }
 
