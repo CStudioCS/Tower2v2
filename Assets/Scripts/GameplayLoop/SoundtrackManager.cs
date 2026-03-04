76d0e2b1aad14b7c211981b9c325d275b1a46e66
pro2p <pierre.fessart@gmail.com>
2026-03-05 00:34:55 +0100

    adding music

---
+++ Assets/Scripts/GameplayLoop/SoundtrackManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SoundtrackManager : MonoBehaviour
+{
+    [SerializeField] AudioSource inGameMusic;
+    [SerializeField] AudioSource lobbyMusic;
+
+    private void Start()
+    {
+        LevelManager.Instance.GameAboutToStart += OnGameAboutToStart;
+        LevelManager.Instance.GameEnded += OnGameEnded;
+        lobbyMusic.Play();
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.Instance.GameAboutToStart -= OnGameAboutToStart;
+        LevelManager.Instance.GameEnded -= OnGameEnded;
+    }
+
+    private void OnGameAboutToStart()
+    {
+        lobbyMusic.Play();
+        inGameMusic.Play();        
+    }
+    private void OnGameEnded()
+    {
+        lobbyMusic.Play();
+        inGameMusic.Stop();
+    }
+}
+
