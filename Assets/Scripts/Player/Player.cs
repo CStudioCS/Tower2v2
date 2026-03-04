0e951ee222f7a30ff125a8b646ab9d6abd211a76
CedrakNedrak <126580442+CedrakNedrak@users.noreply.github.com>
2026-03-05 00:25:45 +0100

    Suppression ligne

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,6 @@
 using LitMotion;
-using LitMotion.Adapters;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
