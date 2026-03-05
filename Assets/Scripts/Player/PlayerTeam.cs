f10cc530f28e0963e02214c22a40423b402fa476
Thomas Bringer <thomas.bringer@madboxgames.io>
2026-03-05 23:41:16 +0100

    Proof of concept. Not functional yet.

--- Assets/Scripts/Player/PlayerTeam.cs
+++ Assets/Scripts/Player/PlayerTeam.cs
@@ -6,38 +6,15 @@ public class PlayerTeam : MonoBehaviour
 {
     public enum Team { Right, Left }
     public Team CurrentTeam { get; private set; } = Team.Left;
-    
-    public event Action TeamChanged;
-    
-    [Header("Colors")]
-    [SerializeField] private Color leftTeamColor;
-    [SerializeField] private Color rightTeamColor;
 
-    private Dictionary<Team, Color> teamColors;
-    public Dictionary<Team, Color> TeamColors
-    {
-        get
-        {
-            teamColors ??= new Dictionary<Team, Color>
-            {
-                { Team.Left, leftTeamColor },
-                { Team.Right, rightTeamColor }
-            };
-            return teamColors;
-        }
-    }
+    public event Action TeamChanged;
 
     [Header("References")]
     [SerializeField] private SpriteRenderer spriteRenderer;
     public int TeamPlayerIndex { get; private set; }
 
-    private void Awake()
-    {
-        UpdateColor();
-    }
-
     private Team CurrentPositionTeam => transform.position.x > 0 ? Team.Right : Team.Left; 
-    
+
     private void Update()
     {
         switch (LevelManager.Instance?.GameState)
@@ -55,15 +32,11 @@ public class PlayerTeam : MonoBehaviour
             SetTeam(newTeam);
     }
 
-
     public void SetTeam(Team team)
     {
         CurrentTeam = team;
-        UpdateColor();
         TeamChanged?.Invoke();
     }
-    
-    private void UpdateColor() => spriteRenderer.color = TeamColors[CurrentTeam];
 
     public void InitTeamPlayerIndex(int teamPlayerIndex)
     {
