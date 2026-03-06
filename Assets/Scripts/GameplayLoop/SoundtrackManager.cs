2c522eff5c7e9a319f69abbf8647dbed9b28e9e7
pro2p <pierre.fessart@gmail.com>
2026-03-06 11:19:31 +0100

    adding fade in and fade out for music

--- Assets/Scripts/GameplayLoop/SoundtrackManager.cs
+++ Assets/Scripts/GameplayLoop/SoundtrackManager.cs
@@ -1,32 +1,47 @@
 using UnityEngine;
+using LitMotion;
+using LitMotion.Adapters;
+using System.Collections;
 
 public class SoundtrackManager : MonoBehaviour
 {
     [SerializeField] AudioSource inGameMusic;
     [SerializeField] AudioSource lobbyMusic;
+    [SerializeField] private float transitionTime;
 
     private void Start()
     {
         LevelManager.Instance.GameAboutToStart += OnGameAboutToStart;
         LevelManager.Instance.GameEnded += OnGameEnded;
-        lobbyMusic.Play();
+        StartCoroutine(SmoothMusicFadeIn(lobbyMusic));
     }
 
     private void OnGameAboutToStart()
     {
-        lobbyMusic.Stop();
-        inGameMusic.Play();        
+        StartCoroutine(SmoothMusicFadeIn(inGameMusic));  
+        StartCoroutine(SmoothMusicFadeOut(lobbyMusic));
     }
 
     private void OnGameEnded()
     {
-        inGameMusic.Stop();
-        lobbyMusic.Play();
+        StartCoroutine(SmoothMusicFadeIn(lobbyMusic));
+        StartCoroutine(SmoothMusicFadeOut(inGameMusic));
     }
 
     private void OnDisable()
     {
         LevelManager.Instance.GameAboutToStart -= OnGameAboutToStart;
         LevelManager.Instance.GameEnded -= OnGameEnded;
+    }
+
+    private IEnumerator SmoothMusicFadeIn(AudioSource targetAudioSource)
+    {
+        targetAudioSource.Play();
+        yield return LMotion.Create(0, 1, transitionTime).Bind(volume => targetAudioSource.volume = volume).ToYieldInstruction();
+    }
+    private IEnumerator SmoothMusicFadeOut(AudioSource actualAudioSource)
+    {
+        yield return LMotion.Create(1, 0, transitionTime).Bind(volume => actualAudioSource.volume = volume).ToYieldInstruction();
+        actualAudioSource.Stop();
     }
 }
