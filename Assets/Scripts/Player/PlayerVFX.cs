29828f8d506fdd61fe671d08d4fc5bb28ec17243
fares <84095553+Fypur@users.noreply.github.com>
2026-03-08 01:11:41 +0100

    Players colliding with each other smoke poof

--- Assets/Scripts/Player/PlayerVFX.cs
+++ Assets/Scripts/Player/PlayerVFX.cs
@@ -4,7 +4,8 @@ using UnityEngine.VFX;
 public class PlayerVFX : MonoBehaviour
 {
     [SerializeField] private Player player;
-    [SerializeField] private VisualEffect vfx;
+    [SerializeField] private VisualEffect grabItemSmokePoof;
+    [SerializeField] private VisualEffect playerCollionSmokePoof;
 
     private void OnEnable()
     {
@@ -13,11 +14,23 @@ public class PlayerVFX : MonoBehaviour
 
     private void OnGrabbedNewItem()
     {
-        vfx.Play();
+        grabItemSmokePoof.Play();
+    }
+
+    private void OnPlayerCollision(Vector2 position)
+    {
+        playerCollionSmokePoof.transform.position = position;
+        playerCollionSmokePoof.Play();
     }
 
     private void OnDisable()
     {
         player.GrabbedNewItem -= OnGrabbedNewItem;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.transform.parent != null && collision.collider.transform.parent.TryGetComponent<Player>(out Player _))
+            OnPlayerCollision(collision.GetContact(0).point);
     }
 }
