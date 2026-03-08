63c4325103811da31ca1d1ef48b9bbd8b6865c35
Thomas Bringer <thomas.bringer@madboxgames.io>
2026-03-08 15:14:50 +0100

    Fixed pause menu interaction with gamepad.

--- Assets/Scripts/UI/Pause/PauseMenu.cs
+++ Assets/Scripts/UI/Pause/PauseMenu.cs
@@ -1,8 +1,15 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UI;
 
 public class PauseMenu: MonoBehaviour
 {
 	[SerializeField] private GameObject pausePanel;
+	[SerializeField] private EventSystem eventSystem;
+	[SerializeField] private Selectable firstSelectable;
 	private bool isPaused;
 
 	private bool PauseKeyPressed => Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7);
@@ -14,13 +21,35 @@ public class PauseMenu: MonoBehaviour
 		if (!LevelManager.InGame)
 			return;
 
-		if (!PauseKeyPressed)
+		if (PauseKeyPressed)
+		{
+			if (isPaused)
+				Resume();
+			else
+				Pause();
 			return;
+		}
+		
+		if (isPaused && eventSystem.currentSelectedGameObject == null && AnyInputPressed)
+		{
+			SelectFirstSelectable();
+		}
+	}
 
-		if (isPaused)
-			Resume();
-		else
-			Pause();
+	private bool AnyInputPressed
+	{
+		get
+		{
+			if (Input.anyKeyDown)
+				return true;
+
+			foreach (Gamepad gamepad in Gamepad.all)
+			{
+				if (gamepad.allControls.OfType<ButtonControl>().Any(button => button.isPressed))
+					return true;
+			}
+			return false;
+		}
 	}
 
 	public void Resume()
@@ -33,7 +62,10 @@ public class PauseMenu: MonoBehaviour
 	private void Pause()
 	{
 		pausePanel.SetActive(true);
+		SelectFirstSelectable();
 		Time.timeScale = 0f;
 		isPaused = true;
 	}
+	
+	private void SelectFirstSelectable() => eventSystem.SetSelectedGameObject(firstSelectable.gameObject);
 }
