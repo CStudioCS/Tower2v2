d82d6f4c68803d5b994ea251c7cc28c0419898cf
mathisramalho-source <mathis.ramalho@gmail.com>
2026-03-08 17:16:14 +0100

    v1

---
+++ Assets/Scripts/Player/PlayerItemCatcher.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerItemCatcher : MonoBehaviour
+{
+    [SerializeField] private PlayerTeam playerTeam;
+    [SerializeField] private Player player;
+    [SerializeField] private float limitVelocity = 2f;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider == null || collider.transform.parent == null)
+            return;
+
+        if(!collider.transform.parent.TryGetComponent<Item>(out Item item) || !collider.transform.parent.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+            return;
+
+        if (item.LastOwner.PlayerTeam == playerTeam)
+            return;
+
+        if (item.LastOwner.PlayerTeam.CurrentTeam != playerTeam.CurrentTeam)
+            return;
+
+        if (item.State != Item.ItemState.Dropped)
+            return;
+
+        if (Mathf.Abs(rb.linearVelocity.magnitude) < limitVelocity)
+            return;
+
+        if (player.IsHolding)
+            return;
+
+        player.GrabItem(item,false);
+    }
+}
