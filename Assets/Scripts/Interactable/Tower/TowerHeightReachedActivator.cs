fada14f500451d65ca2157e97520187b9c078296
Thomas Bringer <thomas.bringer@madboxgames.io>
2026-03-18 03:44:36 +0100

    Polished sails with sprite mask animation and sfx.

--- Assets/Scripts/Interactable/Tower/TowerHeightReachedActivator.cs
+++ Assets/Scripts/Interactable/Tower/TowerHeightReachedActivator.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 public class TowerHeightReachedActivator : MonoBehaviour
 {
 	[SerializeField] private Tower tower;
-	[SerializeField] private GameObject gameObjectToActivate;
 	[SerializeField] private int minimumHeight = 12;
 
+	public event Action HeightReached;
+	
 	private void Start()
 	{
 		tower.PieceBuilt += OnPieceBuilt;
-		LevelManager.Instance.GameAboutToStart += OnGameAboutToStart;
-		Activate(false);
 	}
-
-	private void OnGameAboutToStart() => Activate(false);
-	
-	private void Activate(bool active = true) => gameObjectToActivate.SetActive(active);
 
 	private void OnPieceBuilt()
 	{
-		if (tower.Height >= minimumHeight) Activate();
+		if (tower.Height >= minimumHeight) HeightReached?.Invoke();
 	}
 
 	private void OnDisable()
 	{
 		tower.PieceBuilt -= OnPieceBuilt;
-		LevelManager.Instance.GameAboutToStart -= OnGameAboutToStart;
 	}
 }
