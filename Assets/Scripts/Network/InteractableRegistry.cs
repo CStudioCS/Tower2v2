bd00f1319c17ef3108427b2b35bd6166c47ad7e4
Thomas Bringer <thomas.bringer@madboxgames.io>
2026-05-01 12:55:38 +0200

    Remove all added C# scripts for Copilot review dummy PR

--- Assets/Scripts/Network/InteractableRegistry.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-public static class InteractableRegistry
-{
-    public static Dictionary<int, Interactable> All = new Dictionary<int, Interactable>();
-
-    public static void Register(Interactable interactable) => All[interactable.NetworkId] = interactable;
-    public static void Unregister(Interactable interactable) => All.Remove(interactable.NetworkId);
-}
